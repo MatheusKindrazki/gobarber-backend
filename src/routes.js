557bd5bfd5662f808a4a26c4580da7d3f49c3e60
Matheus Kindrazki <matheus@mentores.com.br>
2019-08-20 23:01:36 -0300

    configuração do eslint

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
 import { Router } from 'express';
 
-const routes = new Router;
+const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Ola Mundo' });
-})
+routes.get('/', (req, res) => res.json({ message: 'Ola Mundo' }));
 
 export default routes;
